dda307ae560738a1c4e8cd14bf1113213d442060
Maksim Omelchenko <omelchenk0.m89@yandex.ru>
2023-05-24 01:42:31 +0700

    1111

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -88,6 +88,7 @@ const login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000,
         httpOnly: true,
+        sameSite: 'none',
       });
       res.send(user);
     })
